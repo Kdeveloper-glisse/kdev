84c41c2ae9078f2c5690f2683316367ebdc8f057
DevK <kdev-consultas@hotmail.com>
2021-02-04 17:44:24 -0500

    udpate form to

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -17,38 +17,36 @@ $(document).ready(function() {
       error.insertAfter(element.parent());
     },
     submitHandler: function(form) {
-     var $form = $(this),
-      username = $form.find('#username').val(),
-      email = $form.find('#email').val(),
-      subject = $form.find('#subject').val(),
-      message = $form.find('#message').val();
+      username = $(form).find('#username').val(),
+      email = $(form).find('#email').val(),
+      subject = $(form).find('#subject').val(),
+      message = $(form).find('#message').val();
     
-    var data = {
-      username: username,
-      sender: email,
-      subject: subject,
-      body: message
-    };
+      var data = {
+        username: username,
+        sender: email,
+        subject: subject,
+        body: message
+      };
     
-    var url = 'https://kdev-app.herokuapp.com/sendmail';
-    $('#result').html('<div style="color:#98c593"><br/>Enviando mensaje, espere por favor...</div>');
- 
-    $.ajax({
-      url: url,
-      type: 'POST',
-      crossDomain: true,
-      dataType:'json',
-      data: data,
-      headers: {  'Access-Control-Allow-Origin': '*' },
-      success: function(jsondata){
-        $('#result').html('<div style="color:#98c593">'+jsondata.message+'</div>');
-        $form[0].reset();
-      },
-      error:function(xhr,status,error){
-        alert(status);
-      }
-    });
+      var url = 'https://kdev-app.herokuapp.com/sendmail';
+      $('#result').html('<div style="color:#98c593"><br/>Enviando mensaje, espere por favor...</div>');
+  
+      $.ajax({
+        url: url,
+        type: 'POST',
+        crossDomain: true,
+        dataType:'json',
+        data: data,
+        headers: {  'Access-Control-Allow-Origin': '*' },
+        success: function(jsondata){
+          $('#result').html('<div style="color:#98c593">'+jsondata.message+'</div>');
+          $(form)[0].reset();
+        },
+        error:function(xhr,status,error){
+          alert(status);
+        }
+      });
     }
-
-});
+  });
 });
